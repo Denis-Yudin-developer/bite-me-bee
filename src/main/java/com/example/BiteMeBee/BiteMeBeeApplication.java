9b3e0d181096d8e9760e64515402dc6dd91f615d
Stepan Mednikov <x-steff@mail.ru>
2022-02-25 13:48:20 +0300

    добавил зависимости, подключил swagger, добавил docker-compose

---
+++ src/main/java/com/example/BiteMeBee/BiteMeBeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.BiteMeBee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiteMeBeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiteMeBeeApplication.class, args);
+	}
+
+}
