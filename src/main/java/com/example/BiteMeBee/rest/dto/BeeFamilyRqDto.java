98379e9c898b2e99936e93969614174c78ddd865
Artyom <divinefridaay@mail.ru>
2022-02-27 23:46:03 +0300

    Добавлена сущность BeeFamily и DTO для неё

---
+++ src/main/java/com/example/BiteMeBee/rest/dto/BeeFamilyRqDto.java
@@ -0,0 +1,32 @@
+package com.example.BiteMeBee.rest.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Входной DTO пчелиной семьи")
+public class BeeFamilyRqDto {
+
+  //Подразумеваю, что пользователь на фронте будет выбирать вид пчёл(название вида уникально)
+  @Size(max = 100, message
+    = "Длинна названия вида должна быть в промежутке от 1 до 100 символов")
+  @NotNull(message = "Не указано название вида")
+  @Schema(description = "Название вида пчелиной семьи")
+  private String beeTypeTitle;
+
+  @NotNull(message = "Не указана заметка о пчелиной семье")
+  @Schema(description = "Заметка о пчелиной семье")
+  private String note;
+
+  @NotNull(message = "Не указан статус жизни пчёл")
+  @Schema(description = "Живая ли пчелиная семья")
+  private Boolean status;
+}
---
+++ src/main/java/com/example/BiteMeBee/rest/dto/BeeFamilyRsDto.java
@@ -0,0 +1,27 @@
+package com.example.BiteMeBee.rest.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Входной DTO пчелиной семьи")
+public class BeeFamilyRsDto {
+
+  @Schema(description = "Идентификатор пчелиной семьи")
+  private Long id;
+
+  @Schema(description = "Название вида пчелиной семьи")
+  private String beeTypeTitle;
+
+  @Schema(description = "Заметка о пчелиной семье")
+  private String note;
+
+  @Schema(description = "Живая ли пчелиная семья")
+  private Boolean status;
+}
