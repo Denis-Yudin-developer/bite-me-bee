212336efd8d8a547e21878774c2c4fea569e2b4d
Stepan Mednikov <stepan.mednikov@corp.codeinside.ru>
2022-03-05 16:01:18 +0300

    CRUD пчелиной семьи

--- src/main/java/com/example/BiteMeBee/rest/dto/BeeFamilyNoteRqDto.java
+++ src/main/java/com/example/BiteMeBee/rest/dto/BeeFamilyNoteRqDto.java
@@ -6,23 +6,19 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Schema(description = "Выходной DTO пчелиной семьи")
-public class BeeFamilyRsDto {
+@Schema(description = "Входной DTO заметки о пчелиной семье")
+public class BeeFamilyNoteRqDto {
 
-    @Schema(description = "Идентификатор", example = "1")
-    private Long id;
-
-    @Schema(description = "Выходной DTO вида пчёл", example = "1")
-    private BeeTypeRsDto beeType;
-
+    @Size(max = 1000, message = "Максимальная длина заметки — 1000 символов")
+    @NotNull(message = "Не указана заметка о пчелиной семье")
     @Schema(description = "Заметка о пчелиной семье",
             example = "Купили на Авито. Продавец Александр, 8(903)123-45-67", maxLength = 1000)
     private String note;
-
-    @Schema(description = "Живёт ли пчелиная семья в улье", example = "true")
-    private Boolean isAlive;
 }
