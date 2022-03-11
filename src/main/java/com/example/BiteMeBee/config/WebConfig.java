a4e536c3ba98af5399f4b5fd61807249e19cb7f3
Stepan Mednikov <stepan.mednikov@corp.codeinside.ru>
2022-03-11 11:52:49 +0300

    #18 мелкие правки

--- src/main/java/com/example/BiteMeBee/config/WebConfig.java
+++ src/main/java/com/example/BiteMeBee/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.example.BiteMeBee.config;
 
 import com.example.BiteMeBee.rest.filter.BeeFamilyFilter;
+import com.example.BiteMeBee.rest.filter.HiveRequestFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -20,6 +22,16 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(getSessionManager())
                 .addPathPatterns("/api/bee_families");
+    }
+
+    @Bean
+    public FilterRegistrationBean<HiveRequestFilter> hivePathLoggingFilter(){
+        FilterRegistrationBean<HiveRequestFilter> registrationBean = new FilterRegistrationBean<>();
+
+        registrationBean.setFilter(new HiveRequestFilter());
+        registrationBean.addUrlPatterns("/api/hives");
+
+        return registrationBean;
     }
 
 }
