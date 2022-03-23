1060d2d49968b26da06e914fd0860401f6dc57a5
Stepan Mednikov <stepan.mednikov@corp.codeinside.ru>
2022-03-23 12:28:55 +0300

    #8 добавил проверку улья на существование

--- app-main/src/main/java/ru/coderiders/BiteMeBee/service/impl/BeeTypeServiceImpl.java
+++ app-main/src/main/java/ru/coderiders/BiteMeBee/service/impl/BeeTypeServiceImpl.java
@@ -57,7 +57,7 @@ public class BeeTypeServiceImpl implements BeeTypeService {
 
         var typeTitle = beeTypeRqDto.getTitle();
         beeTypeRepository.findByTitle(typeTitle)
-                .ifPresent(meterType -> {
+                .ifPresent(found -> {
                     throw new BadRequestException(String.format(BEE_TYPE_ALREADY_EXISTS, typeTitle));
                 });
 
