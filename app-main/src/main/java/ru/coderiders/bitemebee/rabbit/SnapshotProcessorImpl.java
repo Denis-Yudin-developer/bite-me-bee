7e28a242ef21facc558c663d085874e1df6c7c47
Artyom <divinefridaay@mail.ru>
2022-04-03 14:53:53 +0300

    Реализованы тесты WebMvc

--- app-main/src/main/java/ru/coderiders/bitemebee/rabbit/SnapshotProcessorImpl.java
+++ app-main/src/main/java/ru/coderiders/bitemebee/rabbit/SnapshotProcessorImpl.java
@@ -8,7 +8,6 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.coderiders.bitemebee.entity.HiveSnapshot;
 import ru.coderiders.bitemebee.service.impl.HiveServiceImpl;
 import ru.coderiders.bitemebee.service.impl.HiveSnapshotServiceImpl;
-import ru.coderiders.commons.rest.dto.HiveSnapshotRsDto;
 import ru.coderiders.commons.rest.dto.HiveSnapshotGeneratorDto;
 
 @Slf4j
