a51f7070e14070a3e2c32acb2a75aa213c6730b4
denis.yudin <rng48322@gmail.com>
2022-04-19 20:31:14 +0300

    Pull неправильно подтянулся

--- app-commons/src/main/java/ru/coderiders/commons/client/OpenWeatherFeignClient.java
+++ app-commons/src/main/java/ru/coderiders/commons/client/OpenWeatherFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.coderiders.commons.rest.dto.openweather.WeatherDto;
 
-@FeignClient(name = "openWeatherFeignClient", url = "${open-weather.url}")
+@FeignClient(name = "openWeatherFeignClient", url = "${open-weather-client.url}")
 public interface OpenWeatherFeignClient {
     @GetMapping(value = "/data/2.5/weather?q=Penza,ru&APPID=${open-weather.app-id}&lang=ru&units=metric", produces = "application/json")
     WeatherDto getWeather();
